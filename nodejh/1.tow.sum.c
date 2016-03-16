bd21478993d77f786aff593a8034bf16390f84c4
nodejh <replyjh@gmail.com>
2016-03-16 18:30:30 +0800

    第一题通过

---
+++ nodejh/1.tow.sum.c
@@ -0,0 +1,16 @@
+int* twoSum(int* nums, int numsSize, int target) {
+	int i, j;
+	int *res = (int* )malloc(sizeof(int) * 2);
+
+	for (i=0;i<numsSize;i++) {
+		for(j=i+1;j<numsSize;j++) {
+			if ((nums[i] + nums[j]) == target) {
+				res[0] = i;
+				res[1] = j;
+				break;
+			}
+		}
+	}
+	return res;
+}
+
