3fa3e89b4db75e0a183f0f0c2aaa1604f0a9c60a
jibancanyang <jibancanyang@foxmail.com>
2016-03-21 17:13:23 +0800

    this is a assemble file

---
+++ zyc.s
@@ -0,0 +1,60 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 11
+	.globl	_sum
+	.align	4, 0x90
+_sum:                                   ## @sum
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %esi
+	addl	-8(%rbp), %esi
+	movl	%esi, %eax
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+	.globl	_main
+	.align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp3:
+	.cfi_def_cfa_offset 16
+Ltmp4:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp5:
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$1, %edi
+	movl	$2, %esi
+	movl	$0, -4(%rbp)
+	callq	_sum
+	leaq	L_.str(%rip), %rdi
+	movl	%eax, %esi
+	movb	$0, %al
+	callq	_printf
+	xorl	%esi, %esi
+	movl	%eax, -8(%rbp)          ## 4-byte Spill
+	movl	%esi, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"%d\n"
+
+
+.subsections_via_symbols
